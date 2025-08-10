d683df5aff11c11e6c408b407d7c60f4142c7a69
Aniket <joshianiket9@gmail.com>
2025-08-10 19:30:25 +0530

    init commit

---
+++ src/main/java/com/synechron/apis/synechronapis/SynechronApisApplication.java
@@ -0,0 +1,13 @@
+package com.synechron.apis.synechronapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynechronApisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SynechronApisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/synechron/apis/synechronapis/SynechronApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synechron.apis.synechronapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynechronApisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
