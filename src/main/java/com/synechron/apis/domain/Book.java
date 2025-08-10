316e7642ad67676afbc7dec7dd3ccb9bf6c49172
Aniket <joshianiket9@gmail.com>
2025-08-10 19:39:33 +0530

    init commit

---
+++ src/main/java/com/synechron/apis/domain/Book.java
@@ -0,0 +1,49 @@
+package com.synechron.apis.domain;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String isbn;
+    @ManyToMany
+    @JoinTable(name = "Author_Book",joinColumns = @JoinColumn(name="author_Id"),inverseJoinColumns = @JoinColumn(name="book_Id"))
+    private Set<Author> authors;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+}
