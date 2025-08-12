5d034417b5be3226ceb6f453e3a76890755d67ee
Aniket <joshianiket9@gmail.com>
2025-08-12 18:53:41 +0530

    init commit

---
+++ src/main/java/com/synechron/apis/controller/BookController.java
@@ -0,0 +1,4 @@
+package com.synechron.apis.controller;
+
+public class BookController {
+}
---
+++ src/main/java/com/synechron/apis/controller/LibraryController.java
@@ -0,0 +1,10 @@
+package com.synechron.apis.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller("/")
+public class LibraryController {
+
+
+
+}
